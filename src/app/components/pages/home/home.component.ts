2f104e2c9336f8d6d0db0b93648965f0b30a4549
Alex Arruda <alexrodrigoarruda@gmail.com>
2023-05-03 20:01:50 -0300

    Criada a página home e implementados: o resgate de momentos do banco e a filtragem de momentos

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment/moment.service';
+import { IMoment } from 'src/app/interfaces/Moment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  allMoments: IMoment[] = [];
+  moments: IMoment[] = [];
+  faSearch = faSearch;
+  baseApiUrl = environment.baseApiUrl;
+  searchTerm: string = "";
 
+  constructor(
+    private momentService: MomentService,
+  ) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((response) => {
+      const data = response.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString("pt-BR");
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
+
+  search(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title?.toLowerCase().includes(value);
+    });
+  }
 }
--- src/app/services/moment/moment.service.ts
+++ src/app/services/moment/moment.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IMoment } from 'src/app/interfaces/Moment';
+import { IResponse } from 'src/app/interfaces/Response';
 
 import { environment } from 'src/environments/environment';
 
@@ -16,5 +17,9 @@ export class MomentService {
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiURL, formData);
+  }
+
+  getMoments(): Observable<IResponse<IMoment[]>> {
+    return this.http.get<IResponse<IMoment[]>>(this.apiURL);
   }
 }
