44f1688b80e5b4e7302364593ee7595cb28dddef
Alex Arruda <alexrodrigoarruda@gmail.com>
2023-05-04 22:53:25 -0300

    Implementada a funcionalidade de inserção de comentários

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment/moment.service';
 import { IMoment } from 'src/app/interfaces/Moment';
+import { IComment } from 'src/app/interfaces/Comment';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { MessagesService } from 'src/app/services/messages/messages.service';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { CommentService } from 'src/app/services/comment/comment.service';
 
 @Component({
   selector: 'app-moment',
@@ -14,17 +17,33 @@ export class MomentComponent implements OnInit {
   moment?: IMoment;
   baseApiUrl = environment.baseApiUrl;
 
+  commentForm!: FormGroup;
+
   constructor ( 
     private momentService: MomentService,
     private route: ActivatedRoute,
     private messagesService: MessagesService,
     private router: Router,
-    ) {}
+    private commentService: CommentService,
+  ) {}
 
   ngOnInit(): void {
     const id  = Number(this.route.snapshot.paramMap.get("id"));
 
     this.momentService.getMoment(id).subscribe(response => this.moment = response.data);
+  
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
+    });
+  }
+
+  get text() {
+    return this.commentForm.get("text");
+  }
+
+  get username() {
+    return this.commentForm.get("username")!;
   }
 
   removeMoment(id: number) {
@@ -32,6 +51,32 @@ export class MomentComponent implements OnInit {
       next: () => {
         this.messagesService.add("Momento excluído com sucesso!");
         this.router.navigate(["/"]);
+      }
+    });
+  }
+
+  onSubmit(formDirective: FormGroupDirective) {
+    if(this.commentForm.invalid) {
+      return;
+    }
+
+    const id = Number(this.moment!.id);
+
+    const data: IComment = this.commentForm.value;
+
+    data.momentId = id;
+
+    this.commentService.createComment(id, data).subscribe({
+      next: (response) => {
+        this.moment!.comments!.push(response.data);
+
+        this.messagesService.add("Comentário adicionado!");
+      
+        // Limpando o formulário
+        this.commentForm.reset();
+
+        // Limpando o modelo do formulário
+        formDirective.resetForm();
       }
     });
   }
--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IComment } from 'src/app/interfaces/Comment';
+import { IResponse } from 'src/app/interfaces/Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  createComment(id: number, data: IComment): Observable<IResponse<IComment>> {
+    const url = `${this.apiUrl}/${id}/comments`;
+    return this.http.post<IResponse<IComment>>(url, data);
+  }
 }
